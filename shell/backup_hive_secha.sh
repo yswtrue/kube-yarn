5e20df1acbe20bad1b8a47473513cb64e6f45513
yswtrue <a365039311@gmail.com>
2018-06-21 15:11:57 +0800

    feature 细节完善

--- shell/backup_hive_secha.sh
+++ shell/backup_hive_secha.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 databases=$(hive -e 'show databases;')
-mkdir /tmp/hive_export
 for database in $databases;
 do
-    echo -e "CREATE DATABASE IF NOT EXISTS ${database};use ${database};" >> /tmp/hive_export/${database}_secha.sql
+    echo -e "CREATE DATABASE IF NOT EXISTS ${database};use ${database};" > ${database}_secha.sql
     #获取hive表定义
     ret=$(hive -e "use ${database};show tables;"|grep -v _es|grep -v _hb|grep -v importinfo)
 
     for table in $ret;
     do
-        hive -e "use ${database};show create table $table" >> /tmp/hive_export/${database}_secha.sql
-        echo -e ';' >> /tmp/hive_export/${database}_secha.sql
-        echo -e "MSCK REPAIR TABLE $table;"  >> /tmp/hive_export/${database}_secha.sql
+        echo -e "drop if exists table $table;" >> ${database}_secha.sql
+        hive -e "use ${database};show create table $table" >> ${database}_secha.sql
+        echo -e ';' >> ${database}_secha.sql
+        echo -e "MSCK REPAIR TABLE $table;"  >> ${database}_secha.sql
         # listpartitions=`hive -e "use $database; show partitions ${table}"`
         #
         # for tablepart in $listpartitions
         # do
         #    partname=`echo ${tablepart/=/=\"}`
         #    echo $partname
-        #    echo "ALTER TABLE $table ADD PARTITION ($partname\");" >> /tmp/hive_export/${database}_secha.sql
+        #    echo "ALTER TABLE $table ADD PARTITION ($partname\");" >> ${database}_secha.sql
         # done
     done
 
